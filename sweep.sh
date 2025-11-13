cb15a3a3dc709385bf4baf5c165fda90725867ff
Marcello Barylli <marcello.barylli@gmail.com>
2025-11-13 13:30:35 +0000

    sweep update

--- sweep.sh
+++ sweep.sh
@@ -3,7 +3,7 @@
 # This script launches 5 W&B agents in parallel on different GPUs
 # for the specified sweep.
 
-SWEEP_ID="marcello-barylli-growai/boolean_nca_cc/o3fwojtx"
+SWEEP_ID="marcello-barylli-growai/boolean-nca-cc/p8nsl9sk"
 
 echo "Starting agents for sweep: $SWEEP_ID"
 
@@ -13,9 +13,10 @@ echo "Starting agents for sweep: $SWEEP_ID"
 # CUDA_VISIBLE_DEVICES=2 wandb agent $SWEEP_ID &
 
 # squadron 2
-CUDA_VISIBLE_DEVICES=3 wandb agent $SWEEP_ID &
-CUDA_VISIBLE_DEVICES=4 wandb agent $SWEEP_ID &
-# CUDA_VISIBLE_DEVICES=5 wandb agent $SWEEP_ID &
-# CUDA_VISIBLE_DEVICES=7 wandb agent $SWEEP_ID &
+# CUDA_VISIBLE_DEVICES=3 wandb agent $SWEEP_ID &
+# CUDA_VISIBLE_DEVICES=4 wandb agent $SWEEP_ID &
+CUDA_VISIBLE_DEVICES=5 wandb agent $SWEEP_ID &
+CUDA_VISIBLE_DEVICES=6 wandb agent $SWEEP_ID &
+CUDA_VISIBLE_DEVICES=7 wandb agent $SWEEP_ID &
 
 echo " agents launched in the background."
